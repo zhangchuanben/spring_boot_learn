f56677c56df8d9962b6939a11bd4110804654c57
zhangchuanben <798944887@qq.com>
2016-08-14 15:13:52 +0800

    添加了一些乱七八糟的jar包

---
+++ src/main/java/com/spbl/SpblApplication.java
@@ -0,0 +1,12 @@
+package com.spbl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpblApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpblApplication.class, args);
+	}
+}
