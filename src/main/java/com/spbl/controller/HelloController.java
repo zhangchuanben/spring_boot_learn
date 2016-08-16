54c7b0ccb4888dfe5e313e826881641a9587d3fd
zhangchuanben <798944887@qq.com>
2016-08-16 20:23:21 +0800

    Signed-off-by: zhangchuanben <798944887@qq.com>

---
+++ src/main/java/com/spbl/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.spbl.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/home")
+	public String home() {
+		return "hello world";
+	}
+}
