98711cc3d2c1e887426908430c52e62128eba02b
zhangchuanben <798944887@qq.com>
2016-08-21 16:01:36 +0800

    add book controller and del some test

---
+++ src/main/java/com/spbl/web/BookController.java
@@ -0,0 +1,27 @@
+package com.spbl.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spbl.domain.Book;
+import com.spbl.repository.BookRepository;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@RequestMapping(value="", method = RequestMethod.GET)
+	public Iterable<Book> getAllBooks() {
+		return bookRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/{isbn}", method = RequestMethod.GET)
+	public Book getByIsbn(@PathVariable String isbn) {
+		return bookRepository.findByIsbn(isbn);
+	}
+}
