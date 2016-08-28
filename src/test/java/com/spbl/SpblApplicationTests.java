a5122ff2f96225a7b609403a861587b1f5a70177
zhangchuanben <798944887@qq.com>
2016-08-28 14:39:24 +0800

    test

--- src/test/java/com/spbl/SpblApplicationTests.java
+++ src/test/java/com/spbl/SpblApplicationTests.java
@@ -1,16 +1,55 @@
 package com.spbl;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.spbl.domain.Book;
+import com.spbl.repository.BookRepository;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment=WebEnvironment.DEFINED_PORT)
 public class SpblApplicationTests {
-
+	@Autowired
+	private WebApplicationContext context;
+	@Autowired
+	private BookRepository repository;
+	@Value("${local.server.port}")
+	private int port;
+	
+	private MockMvc mockMvc;
+	private TestRestTemplate restTemplate = new TestRestTemplate();
+	@Before
+	public void setupMockMvc() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
+	
+	
+	@Rollback // 支持事务回滚
 	@Test
 	public void contextLoads() {
+		/*Iterable<Book> books = repository.findAll();
+		books.forEach(book -> {
+			repository.delete(book);
+		});*/
+		/*Book book = new Book();
+		book.setDescription("哈利波特");
+		book.setIsbn("abcd");
+		book.setTitle("哈利波特全集");
+		repository.save(book);*/
+		assertEquals(1, repository.count());
 	}
 
 }
